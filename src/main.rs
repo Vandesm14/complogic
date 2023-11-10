33465bc7d6931b6e57f93bde3617d86c8aa7f597
Shane Vandegrift <vandesm14@gmail.com>
2023-11-09 19:48:22 -0500

    no traits required

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::rc::Rc;
+
+type Pins = HashMap<usize, bool>;
+
+#[derive(Debug)]
+enum Gate {
+  And {
+    inputs: [usize; 2],
+    outputs: [usize; 1],
+  },
+  Not {
+    inputs: [usize; 1],
+    outputs: [usize; 1],
+  },
+}
+
+impl Gate {
+  fn eval(&self, pins: &Pins) -> Pins {
+    match self {
+      Self::And { inputs, outputs } => {
+        let [pin_a, pin_b] = inputs;
+
+        let a = pins.get(pin_a).unwrap();
+        let b = pins.get(pin_b).unwrap();
+        let result = *a && *b;
+
+        let mut new_pins = pins.clone();
+        new_pins.insert(outputs[0], result);
+
+        new_pins
+      }
+      Self::Not { inputs, outputs } => {
+        todo!()
+      }
+    }
+  }
+}
+
+#[derive(Debug)]
+struct World {
+  pins: Pins,
+
+  /// The components within a world
+  components: Vec<Gate>,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let pins: Pins = Pins::from_iter(vec![(0, true), (1, true)]);
+  let and_gate = Gate::And {
+    inputs: [0, 1],
+    outputs: [2],
+  };
+
+  let result = and_gate.eval(&pins);
+
+  println!("{:?}", result);
 }
