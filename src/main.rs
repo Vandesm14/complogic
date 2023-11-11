cf231a44692735f417ff238c6989330a8dd1f652
Shane Vandegrift <vandesm14@gmail.com>
2023-11-11 15:40:43 -0500

    fun: 4 bit adder

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,78 @@
-use complogic::Simulation;
+use std::rc::Rc;
+
+use complogic::{FullAdder, Simulation};
+
+fn number_to_bin_vec(number: usize, size: usize) -> Vec<bool> {
+  let mut vec = vec![];
+  let mut number = number;
+  while number > 0 {
+    vec.push(number % 2 == 1);
+    number /= 2;
+  }
+
+  while vec.len() < size {
+    vec.push(false);
+  }
+
+  vec.reverse();
+  vec
+}
 
 fn main() {
-  let mut simulation = Simulation::new(2);
-  assert_eq!(simulation.registers.len(), 2);
+  let mut simulation = Simulation::new(8);
+  let [a4, a3, a2, a1, b4, b3, b2, b1] = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  simulation.compile(vec![]);
-  assert_eq!(simulation.registers.len(), 2);
+  let full_adder_1 = Rc::new(FullAdder {
+    a: a1,
+    b: b1,
+    cin: simulation.alloc(),
+    s: simulation.alloc(),
+    cout: simulation.alloc(),
+  });
+  let full_adder_2 = Rc::new(FullAdder {
+    a: a2,
+    b: b2,
+    cin: full_adder_1.cout,
+    s: simulation.alloc(),
+    cout: simulation.alloc(),
+  });
+  let full_adder_3 = Rc::new(FullAdder {
+    a: a3,
+    b: b3,
+    cin: full_adder_2.cout,
+    s: simulation.alloc(),
+    cout: simulation.alloc(),
+  });
+  let full_adder_4 = Rc::new(FullAdder {
+    a: a4,
+    b: b4,
+    cin: full_adder_3.cout,
+    s: simulation.alloc(),
+    cout: simulation.alloc(),
+  });
 
-  assert_eq!(simulation.alloc(), 2);
-  assert_eq!(simulation.alloc(), 3);
+  simulation.compile(vec![
+    full_adder_1.clone(),
+    full_adder_2.clone(),
+    full_adder_3.clone(),
+    full_adder_4.clone(),
+  ]);
 
-  simulation.compile(vec![]);
-  assert_eq!(simulation.registers.len(), 4);
+  let a = 0b0101;
+  let b = 0b0011;
+
+  simulation.run(
+    &number_to_bin_vec(a, 4)
+      .into_iter()
+      .chain(number_to_bin_vec(b, 4))
+      .collect::<Vec<_>>(),
+  );
+
+  println!("Expected: {:04b}", a + b);
+
+  println!("1: {}", simulation.register(full_adder_1.s));
+  println!("2: {}", simulation.register(full_adder_2.s));
+  println!("3: {}", simulation.register(full_adder_3.s));
+  println!("4: {}", simulation.register(full_adder_4.s));
+  println!("cout: {}", simulation.register(full_adder_4.cout));
 }
