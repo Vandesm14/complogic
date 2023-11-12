341fa7f75e8a0ce69b59eb26d96106fdcd30fa12
Shane Vandegrift <vandesm14@gmail.com>
2023-11-12 17:17:12 -0500

    use complogic as name

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'complogic';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './complogic.js',
+  './complogic_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
