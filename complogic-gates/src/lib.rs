5119770491e9df3daf7a425b65fd183bec106fa8
Shane <vandesm14@gmail.com>
2024-08-04 11:34:10 -0400

    run Rust in Rust using WASM

--- complogic-gates/src/lib.rs
+++ complogic-gates/src/lib.rs
@@ -1,11 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-  alert(&format!("Hello, {}!", name));
+pub fn add_one(number: i32) -> i32 {
+  number + 1
 }
--- complogic/src/main.rs
+++ complogic/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-  println!("Hello, world!");
+use wasmer::{imports, Instance, Module, Store, Value};
+
+fn main() -> anyhow::Result<()> {
+  let module_wasm =
+    include_bytes!("../../complogic-gates/pkg/complogic_gates_bg.wasm");
+
+  let mut store = Store::default();
+  let module = Module::new(&store, module_wasm)?;
+  // The module doesn't import anything, so we create an empty import object.
+  let import_object = imports! {};
+  let instance = Instance::new(&mut store, &module, &import_object)?;
+
+  let add_one = instance.exports.get_function("add_one")?;
+  let result = add_one.call(&mut store, &[Value::I32(42)])?;
+  assert_eq!(result[0], Value::I32(43));
+
+  Ok(())
 }
