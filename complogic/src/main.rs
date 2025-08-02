e13f38e84fbab311f52f53553ed14257837fd1bc
Shane <vandesm14@gmail.com>
2025-08-02 19:59:16 -0400

    test and gate

--- complogic/src/main.rs
+++ complogic/src/main.rs
@@ -12,5 +12,17 @@ fn main() -> anyhow::Result<()> {
   let result = simulation.execute("std", "counter", 0);
   assert_eq!(result, Some(2));
 
+  let result = simulation.execute("std", "and", 0b00);
+  assert_eq!(result, Some(0));
+
+  let result = simulation.execute("std", "and", 0b01);
+  assert_eq!(result, Some(0));
+
+  let result = simulation.execute("std", "and", 0b10);
+  assert_eq!(result, Some(0));
+
+  let result = simulation.execute("std", "and", 0b11);
+  assert_eq!(result, Some(1));
+
   Ok(())
 }
